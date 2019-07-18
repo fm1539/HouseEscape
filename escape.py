21096211ddc90b22fa9bc1dd8ed8fccaf5fc97ab
EC2 Default User <ec2-user@ip-172-31-21-7.us-east-2.compute.internal>
2019-07-18 18:39:59 +0000

    Initial project commit

---
+++ escape.py
@@ -0,0 +1,22 @@
+print("Welcome to the escape room challenge!\n" "You wake up in an empty room with 4 doors.\n Each door leads to a different room: A for Green, B for blue, C for Yellow, D for Orange.\n One of the rooms leads to the path to escape. You have 3 attempts in the first room, and number of attempts decreases as you move from room to room. \nChoose wisely!\n")
+
+start = input("Choose from the following doors: \n A\n B\n C\n D \n \n")
+start = start.upper()
+
+print(start)
+
+if start == "A":
+    print("Answer this riddle in under 3 attempts:\n What has a head, a tail, is brown, and has no legs?\n Hints: \n You see it everyday, but ignore it most of the time.\n It makes the world go round.\n Abraham Lincoln.\n")
+    answer = input('What is your guess?\n')
+    answer = answer.title()
+    attempts_1 = 3
+    if answer == 'Penny':
+        print('Penny is the right answer! you may move on!\n ')
+        move_n_1 = input('Enter e to move to room 2\n')
+    else:
+        while (answer!= 'Penny') and (attempts_1 != 0):
+            print('Sorry ' + answer + " is not the correct answer.")
+            attempts_1 -= 1
+            print("You have " + str(attempts_1 + ' attempts left.')
+            answer = input('What is your guess?\n')
+        
