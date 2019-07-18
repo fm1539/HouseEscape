a31d4e93b3acb95f4c27ec7b45fa56d2fed9518d
EC2 Default User <ec2-user@ip-172-31-21-7.us-east-2.compute.internal>
2019-07-18 19:23:12 +0000

    fourth mini project commit

--- escape.py
+++ escape.py
@@ -24,6 +24,66 @@ if start == "A":
                 attempts_1 -= 1
                 print("You have " + str(attempts_1) + ' attempts left.')
             answer = input('What is your guess?\n')
+if start == 'B':
+    print("Can you name three consecutive days without using the words Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday?\n (Seperate answers using only commas")
+    answer = input('What is your guess?\n')
+    answer = answer.title()
+    attempts_1 = 3
+    if answer == 'Yesterday, Today, Tomorrow':
+        print('That is the right answer! you may move on!\n ')
+        move_n_1 = input('Enter e to move to room 2\n')
+    else:
+        while (answer!= 'Yesterday, Today, Tomorrow') or (attempts_1 > 1):
+            
+            if attempts_1 == 1:
+                print('Sorry, you are trapped forever.')
+                break
+            else:
+                print('Sorry ' + answer + " is not the correct answer.")
+                attempts_1 -= 1
+                print("You have " + str(attempts_1) + ' attempts left.')
+            answer = input('What is your guess?\n')
+if start == 'C':
+    print("David's father has three sons : Snap, Crackle and _____ ?")
+    answer = input('What is your guess?\n')
+    answer = answer.title()
+    attempts_1 = 3
+    if answer == 'David':
+        print('That is the right answer! you may move on!\n ')
+        move_n_1 = input('Enter e to move to room 2\n')
+    else:
+        while (answer!= 'David') or (attempts_1 > 1):
+            
+            if attempts_1 == 1:
+                print('Sorry, you are trapped forever.')
+                break
+            else:
+                print('Sorry ' + answer + " is not the correct answer.")
+                attempts_1 -= 1
+                print("You have " + str(attempts_1) + ' attempts left.')
+            answer = input('What is your guess?\n')
+if start == 'D':
+    print("What comes down but never goes up")
+    answer = input('What is your guess?\n')
+    answer = answer.title()
+    attempts_1 = 3
+    if answer == 'Rain':
+        print('Rain is the right answer! you may move on!\n ')
+        move_n_1 = input('Enter e to move to room 2\n')
+    else:
+        while (answer!= 'David') or (attempts_1 > 1):
+            
+            if attempts_1 == 1:
+                print('Sorry, you are trapped forever.')
+                break
+            else:
+                print('Sorry ' + answer + " is not the correct answer.")
+                attempts_1 -= 1
+                print("You have " + str(attempts_1) + ' attempts left.')
+            answer = input('What is your guess?\n')
+
+    
+            
             
             
         
